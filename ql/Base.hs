5ab14b2db4221fb58393745ddb61ce3cb4169cac
Rémi Vion <vion.remi@gmail.com>
2016-05-11 16:52:52 +0200

    misc changes

--- ql/Base.hs
+++ ql/Base.hs
@@ -1,20 +1,18 @@
 module Base (module X) where
 
 import Control.Monad.Trans.Except              as X (ExceptT)
-import Control.Monad.Trans.Except (ExceptT)
-import Data.Aeson                              as X(FromJSON, ToJSON, encode, Value)
+import Data.Aeson                              as X (FromJSON, ToJSON, encode, Value)
 import Data.ByteString.Lazy                    as X (toStrict)
-import Data.List                               as X(intercalate)
+import Data.List                               as X (intercalate)
 import Data.Swagger                            as X (ToSchema)
 import Data.Text                               as X (Text)
 import Data.Text.Encoding                      as X (decodeUtf8)
-import GHC.Generics                            as X(Generic)
-import Network.Wai                             as X(Application)
-import Network.Wai.Handler.Warp                as X(run)
+import GHC.Generics                            as X (Generic)
+import Network.Wai                             as X (Application)
+import Network.Wai.Handler.Warp                as X (run)
 import Network.Wai.Middleware.RequestLogger    as X (logStdoutDev)
 import Servant                                 as X
-import Servant.JS                              as X
-import Servant.Swagger                         as X
+import Servant.JS                              as X (jsForAPI,vanillaJS)
+import Servant.Swagger                         as X (toSwagger)
 import Data.Swagger.Internal                   as X (Swagger)
 import Data.Swagger.Internal.Schema            as X (declareNamedSchema)
--- import Servant.HTML.Lucid                      as X
